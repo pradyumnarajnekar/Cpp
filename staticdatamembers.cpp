200e9df84264919966ae7310dded240ac15f4ebc
प्रद्युम्न राज़नेकर <112924476+pradyumnarajnekar@users.noreply.github.com>
2022-09-22 09:09:15 +0530

    Static Data Members

---
+++ staticdatamembers.cpp
@@ -0,0 +1,41 @@
+// Write a cpp program using static data members
+
+#include <iostream>
+using namespace std;
+class item
+{
+    static int count;
+    int number;
+    public:
+        void getdata(int a)
+        {
+            number = a;
+            count++;
+        }
+        void getcount(void)
+        {
+            cout<<"Count : "<<count<<"\n";
+        }
+        
+};
+
+int item::count;
+int main()
+{
+    item a, b, c; //a, b, c = objects   item = class
+    a.getcount();
+    b.getcount();
+    c.getcount();
+    
+    a.getdata(100);
+    b.getdata(200);
+    c.getdata(300);
+    
+    cout<<"After reading data \n";
+    
+    a.getcount();
+    b.getcount();
+    c.getcount();
+    
+    return 0;
+}
